0854711cdb382a359d90b96f3aa6676a894c6ea8
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-30 13:31:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/fade-in-fade-out-post-title/trunk@579165 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- help.php
+++
@@ -1,9 +0,0 @@
-<h2>Plugin configuration option</h2>
-<ol>
-<li>Drag and drop the widget</li>
-<li>Add the plugin in the posts or pages</li>
-<li>Add directly in the theme</li>
-</ol>
-<ul>
-<li>Note: Check official website for more info <a href="http://www.gopipulse.com/work/2011/07/31/fade-in-fade-out-post-title-wordpress-plugin/" target="_blank">click here</a></li>
-</ul>
